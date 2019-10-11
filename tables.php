af1729f1cc35b12e7cb84678a0b3ed6d482da75e
William Tiago Velho Da Silva Goebel <43004@UNIDAVI.LOCAL>
2019-10-10 21:43:36 -0300

    template

---
+++ tables.php
@@ -0,0 +1,70 @@
+<!-- Page Heading -->
+<h1 class="h3 mb-2 text-gray-800">Tables</h1>
+<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+  <div class="card-header py-3">
+  <a href="#" class="btn btn-primary btn-icon-split m-1">
+    <span class="icon text-white-50">
+      <i class="fas fa-flag"></i>
+    </span>
+    <span class="text">Incluir</span>
+  </a>
+  <a href="#" class="btn btn-primary btn-icon-split m-1">
+    <span class="icon text-white-50">
+      <i class="fas fa-flag"></i>
+    </span>
+    <span class="text">Editar</span>
+  </a>
+  <a href="#" class="btn btn-primary btn-icon-split m-1">
+    <span class="icon text-white-50">
+      <i class="fas fa-flag"></i>
+    </span>
+    <span class="text">Visualizar</span>
+  </a>
+  <a href="#" class="btn btn-danger btn-icon-split m-1">
+    <span class="icon text-white-50">
+      <i class="fas fa-flag"></i>
+    </span>
+    <span class="text">Excluir</span>
+  </a>
+  </div>
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr>
+            <td>Tiger Nixon</td>
+            <td>System Architect</td>
+            <td>Edinburgh</td>
+            <td>61</td>
+            <td>2011/04/25</td>
+            <td>$320,800</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
